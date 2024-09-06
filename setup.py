96d3298d7dffc3df3e02101e3c595e5bcfbd27c7
abebus <90478897+abebus@users.noreply.github.com>
2024-09-07 01:29:19 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,8 +15,12 @@ setup(
     classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.8",
         "Operating System :: OS Independent",
         "Intended Audience :: Developers",
     ],
-    python_requires=">=3.12",
+    python_requires=">=3.8",
 )

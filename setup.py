b01bb6b54922c1b61d776c93db67e4eaf4af1f6b
abebus <90478897+abebus@users.noreply.github.com>
2024-09-07 01:40:54 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup, find_packages
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 DESCRIPTION = "Scrapy extension for monitoring spider."
 
 setup(
     name="spider_info_webservice",
     description=DESCRIPTION,
-    long_description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author="abebus",
     author_email="anamaev263@gmail.com",
     version="0.0.3",
